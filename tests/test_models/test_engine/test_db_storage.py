9cedd60ba76805d30c35d07291219624811db332
JeremyWarui <jerywarne7@gmail.com>
2023-05-01 20:21:04 +0300

    Add tests for get and couont functions

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,16 +87,18 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
-    # def test_db_get(self):
-    #     """check if instance found"""
-    #     new_obj = State(name="Bali")
-    #     found = models.storage.get(new_obj, "some_id")
-    #     self.assertIsNone(found)
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_db_get(self):
+        """check if instance found"""
+        new_obj = State(name="Bali")
+        found = models.storage.get(new_obj, "some_id")
+        self.assertIsNone(found)
 
-    # def test_count(self):
-    #     """test count of objs"""
-    #     models.storage.reload()
-    #     all_items = models.storage.count()
-    #     self.assertIsInstance(all_items, int)
-    #     class_cnt = models.storage.count(State)
-    #     self.assertIsInstance(class_cnt, int)
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """test count of objs"""
+        models.storage.reload()
+        all_items = models.storage.count()
+        self.assertIsInstance(all_items, int)
+        class_cnt = models.storage.count(State)
+        self.assertIsInstance(class_cnt, int)
